7c04b8fbf960499df3c1e5441a06d18848cdeeb7
solidnerd <niclas@mietz.io>
2019-07-06 11:24:26 +0200

    Add a little sleep to let tests pass

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 set -e
+WAIT_TIME=30
+echo "Wait $WAIT_TIME seconds before starting tests..."
+sleep $WAIT_TIME
 echo "Start Tests: "
-
 curl --retry 15 --retry-delay 5 --retry-connrefused http://bookstack/login
 
 exit 0
