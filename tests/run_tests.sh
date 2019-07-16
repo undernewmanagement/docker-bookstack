3a55464fc4654010cbdb0ef0567874e6debd5761
solidnerd <niclas@mietz.io>
2019-07-16 13:10:00 +0200

    Test lookup

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -3,6 +3,7 @@ set -e
 WAIT_TIME=30
 echo "Wait $WAIT_TIME seconds before starting tests..."
 sleep $WAIT_TIME
+nslookup bookstack
 echo "Start Tests: "
 curl --retry 15 --retry-delay 5 --retry-connrefused http://bookstack.tasks/login
 
